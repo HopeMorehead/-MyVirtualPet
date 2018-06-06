3369b12a3e65544bf2b2786a55014fdaeea275dc
hopemorehead <SplendidWonder@gmail.com>
2018-06-06 18:07:52 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyVirtualPet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            VirtualPet dog = new VirtualPet("Bob","Gold","dog",200);
+            VirtualPet cat = new VirtualPet();
+            Console.WriteLine(dog.Feed());
+            Console.WriteLine(dog.Feed());
+               dog.Play();
+
+
+            // can't be done  Console.WriteLine( dog.Hungry());
+        }
+    }
+}
---
+++ VirtualPet.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyVirtualPet
+{
+    class VirtualPet
+
+    {
+        internal string Name     {get; set;}
+        internal string Color    {get; set;}
+        internal string Species  {get; set;}
+        internal int HungerLevel {get; set;}
+        internal int EnergyLevel {get; set;}
+
+
+     
+
+        internal VirtualPet()
+        {
+
+        }
+    
+        internal VirtualPet(string Name,string Color, string Species,int HungerLevel)
+        {
+            this.Name = Name;
+            this.Color = Color;
+            this.Species = Species;
+            this.HungerLevel = HungerLevel;
+        }
+
+
+
+        public int Feed()
+        {
+            HungerLevel += 5;
+
+            return HungerLevel;
+        }
+        public void Play()
+        {
+            HungerLevel -= 5;
+            EnergyLevel += 10;
+            Console.WriteLine($"{Name} Hunger:{HungerLevel}");
+            Console.WriteLine($"{Name} Hunger:{EnergyLevel}");            
+
+        }
+    }
+}
